a1a85aecd40e47f92d0abe181962c8cc99113f1d
Yousuf24100286 <24100286@lums.edu.pk>
2023-03-28 12:56:31 +0500

    App.js file Added

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: __dirname+'/.env' });
+const express = require('express');
+require('express-async-errors');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const routes = require('./routes');
+const unknownEndpoint = require('./middlewares/unknownEndpoint');
+const { ErrorHandler, handleError } = require('./middlewares/errorHandler');
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(compression());
+app.use(helmet());
+
+app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.send('<h1>Server is running</h1>');
+});
+
+app.use(unknownEndpoint);
+app.use(handleError);
+
+module.exports = app;
