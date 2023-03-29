58d9513179908f58e7c572aa8e7a29d053b98f05
Yousuf24100286 <24100286@lums.edu.pk>
2023-03-29 15:38:38 +0500

    User Service Added, Auth Completed ig

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -4,20 +4,20 @@ const jwt = require('jsonwebtoken');
 const validateUser = require('../utils/validate').validateUser
 const {createToken} = require('../utils/jwtToken');
 const {getUserByEmail, getUserByUserName, addUser } = require('../services/user');
+const logger = require('../utils/logger');
 const ErrorHandler = require('../middlewares/errorHandler').ErrorHandler;
-
 
 
 class AuthService {
   async createUser(user) {
     try {
+      logger.info('Service: Auth - Call: createUser')
       const {first_name, last_name, username, email, password, telephone } = user;
       if(!first_name || !last_name || !username || !email || !password || !telephone){
         throw new ErrorHandler(400, 'Please enter all fields');  
       }
       if(validateUser(email,password)){
-        const salt = await bcrypt.genSalt(process.env.HASH_SALT_KEY) ;
-        const hash = await bcrypt.hash(password, salt) ;
+        const hash = await bcrypt.hash(password, parseInt(process.env.HASH_SALT_KEY) ) ;
   
         const UserByEmail = await getUserByEmail(email) ;
         const UserByUserName = await getUserByUserName(username) ;
@@ -37,6 +37,7 @@ class AuthService {
           telephone: telephone,
           priv: 'Customer'
         }) ;
+        logger.info('newUser', newUser);
 
   
         const token = createToken({
@@ -44,6 +45,7 @@ class AuthService {
           username: newUser.username,
           email: newUser.email
         });
+        logger.info('token', token);
         return {token: token, user: newUser} ;
       } else {
         throw new ErrorHandler(400, 'Please enter valid email and password');
@@ -54,20 +56,39 @@ class AuthService {
     }
   }
 
-
-
-
-
-
-
-
-
-
-
+  async loginUser(user) {
+    try {
+      logger.info('Service: Auth - Call: loginUser')
+      const {email, password} = user;
+      if(!email || !password){
+        throw new ErrorHandler(400, 'Please enter all fields');  
+      }
+      if(validateUser(email,password)){
+        const UserByEmail = await getUserByEmail(email) ;
+        if(!UserByEmail){
+          throw new ErrorHandler(400, 'Email does not exist');
+        }
+        const isMatch = await bcrypt.compare(password, UserByEmail.password_hash);
+        if(!isMatch){
+          throw new ErrorHandler(400, 'Invalid credentials');
+        }
+        const token = createToken({
+          id: UserByEmail.id,
+          username: UserByEmail.username,
+          email: UserByEmail.email
+        });
+        return {token: token, user: UserByEmail} ;
+      } else {
+        throw new ErrorHandler(400, 'Please enter valid email and password');
+      }
+    }
+    catch (error) {
+      throw new ErrorHandler(error.statusCode, error.message);
+    }
+  }
 
 
 }
-
 
 
 module.exports = new AuthService();
--- backend/services/user.js
+++ backend/services/user.js
@@ -1,34 +1,137 @@
-const Users = require('../database/models/users')
+const Sequelize = require('sequelize');
+const logger = require('../utils/logger');
+const sequelize = new Sequelize(process.env.DEV_PGDATABASE_URL);
+const Users = require('../database/models/users')(sequelize, Sequelize.DataTypes)
 
-
-exports.getUserByUserName = async(username) => {
-  const user = await Users.findOne({
-    where: {
-      username: username
+class UserService {
+  async getUserByID(id) {
+    try {
+      logger.info('Service: User - Call: getUserByID')
+      const user = await Users.findOne({
+        where: {
+          id: id
+        }
+      })
+      return user;
+    } catch (error) {
+      throw new ErrorHandler(error.statusCode, error.message);
     }
-  })
-  return user;
-}
+  }
 
-exports.getUserByEmail = async(email) => {
-  const user = await Users.findOne({
-    where: {
+  async getUserByUserName(username) {
+    try {
+      logger.info('Service: User - Call: getUserByUserName')
+      const user = await Users.findOne({
+        where: {
+          username: username
+        }
+      })
+      return user;
+    } catch (error) {
+      throw new ErrorHandler(error.statusCode, error.message);
+    }
+  }
+  async getUserByEmail(email) {
+    try {
+      logger.info('Service: User - Call: getUserByEmail')
+      const user = await Users.findOne({
+        where: {
+          email: email
+        }
+      })
+      return user;
+    }
+    catch (error) {
+      throw new ErrorHandler(error.statusCode, error.message);
+    }
+  }
+  async addUser(user) {
+    logger.info('Service: User - Call: addUser')
+    const newUser = await Users.create({
+      username: user.username,
+      password_hash: user.password_hash,
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      telephone: user.telephone,
+      priv: 'Customer'
+    })
+    return newUser;
+  }
+    
+  async updateEmail(id, email) {
+    logger.info('Service: User - Call: updateEmail')
+    const user = await Users.update({
       email: email
-    }
-  })
-  return user;
+    }, {
+      where: {
+        id: id
+      }
+    })
+    return user;
+  }
+
+  async updateUsername(id, username) {
+    logger.info('Service: User - Call: updateUsername')
+    const user = await Users.update({
+      username: username
+      }, {
+        where: {
+          id: id
+        }
+      })
+    return user;
+  }
+
+  async updateFirstName(id, first_name) {
+    logger.info('Service: User - Call: updateFirstName')
+    const user = await Users.update({
+      first_name: first_name
+    }, {
+      where: {
+        id: id
+      }
+    })
+    return user;
+  }
+
+  async updateLastName(id, last_name) {
+    logger.info('Service: User - Call: updateLastName')
+    const user = await Users.update({
+      last_name: last_name
+    }, {
+      where: {
+        id: id
+      }
+    })
+    return user;
+  }
+
+  async updateTelephone(id, telephone) {
+    logger.info('Service: User - Call: updateTelephone')
+    const user = await Users.update({
+      telephone: telephone
+    }, {
+      where: {
+        id: id
+      }
+    })
+    return user;
+  }
+
+  async updatePassword(id, password_hash) {
+    logger.info('Service: User - Call: updatePassword')
+    const user = await Users.update({
+      password_hash: password_hash
+    }, {
+      where: {
+        id: id
+      }
+    })
+    return user;
+  }
+
 }
 
 
-exports.addUser = async(user) => {
-  const newUser = await Users.create({
-    username: user.username,
-    password_hash: user.password_hash,
-    email: user.email,
-    first_name: user.first_name,
-    last_name: user.last_name,
-    telephone: user.telephone,
-    priv: 'Customer'
-  })
-  return newUser;
-}
+module.exports = new UserService();
