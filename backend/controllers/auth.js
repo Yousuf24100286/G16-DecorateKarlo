2929b6cc3abdfe239976c88f18ef9b9fc3e7557b
Yousuf24100286 <24100286@lums.edu.pk>
2023-03-29 15:37:34 +0500

    Auth & Users Controllers

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,17 @@
+const logger = require('../utils/logger');
+
 authService = require('../services/auth');
 
 const createUser = async (req, res) => {
+  logger.info('createUser', req.body);
   const {token , user} = await authService.createUser(req.body);
   res.status(201).json({token, user});
 }
 
-
 const loginUser = async (req, res) => {
-
+  logger.info('Controller: Auth - Call: loginUser');
+  const {token , user} = await authService.loginUser(req.body);
+  res.status(200).json({token, user});
 }
 
 module.exports = {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,39 +1,57 @@
-const User = require('../database/models/users');
-const hashPassword = require('../utils/hashpassword') ;
+const logger = require('../utils/logger');
+const userService = require('../services/user');
 
-
-exports.createUser = async (req, res, next) => {
-  try {
-    const {first_name, last_name, username, email, password} = req.body;
-    // check whether user exists by username
-    const user = User.findOne({where: {username}}) || User.findOne({where: {email}});
-    if(user){
-      res.status(400).json({msg: 'User already exists'});
-    }
-    const hash = await hashPassword(password) ;
-    const newUser = new User({
-      first_name,
-      last_name,
-      username,
-      email,
-      hash
-    });
-    res.status(201).json({message:'User Created successfully'}) ;      
-  } catch (error) {
-    next(error) ;
-  }
+const getUserByID = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: getUserByID');
+  const user =  await userService.getUserByID(id);
+  res.status(200).json(user);
 }
 
-exports.getUserByID = async (req, res, id, next) => {
-  try {
-    const user = User.findOne({where: {id}}) ;
-    if(user){
-      res.status(200).json({user}) ;
-    }else{
-      res.status(404).json({message: 'User not found'}) ;
-    }  
-  } catch (error) {
-    next(error) ;
-  }
+const updateEmail = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updateEmail');
+  const user = await userService.updateEmail(id, req.body.email);
+  res.status(200).json(user);
 }
+
+const updateUsername = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updateUsername')
+  const user = await user.userService.updateUsername(id, req.body.email)
+  res.status(200).json(user) ;
+}
+
+const updateFirstName = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updateFirstName')
+  const user = await userService.updateFirstName(id, req.body.first_name)
+  res.status(200).json(user) ;
+}
+
+const updateLastName = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updateLastName')
+  const user = await userService.updateLastName(id, req.body.last_name)
+  res.status(200).json(user) ;
+}
+
+const updateTelephone = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updateTelephone')
+  const user = await userService.updateTelephone(id, req.body.telephone)
+  res.status(200).json(user) ;
+}
+
+const updatePassword = async (req, res, id, next) => {
+  logger.info('Controller: User - Call: updatePassword')
+  const password_hash = await bcrypt.hash(req.body.password, process.env.HASH_SALT_KEY);
+  const user = await userService.updatePassword(id, password_hash)
+  res.status(200).json(user) ;
+}
+
+module.exports = {
+  getUserByID,
+  updateEmail,
+  updateUsername,
+  updateFirstName,
+  updateLastName,
+  updateTelephone,
+  updatePassword
+}
+
 
