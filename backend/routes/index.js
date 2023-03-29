5dc781a4f729b0541ad9eef79db493be829d74a6
Yousuf24100286 <24100286@lums.edu.pk>
2023-03-29 15:38:04 +0500

    User Routes Added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 
 router.use('/auth', require('./auth'));
-
+router.use('/user', require('./user'));
 
 
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../database/models/users')
+const user = require('../controllers/user');
 
-// @route   POST api/users/register
-// @desc    Register user
-// @access  Public
-// redirect to createUser from users.js
-router.post('/signup', (req, res) => {
-  const {first_name, last_name, username, email, password} = req.body;
-  
-  if(!first_name || !last_name || !username || !email || !password){
-    return res.status(400).json({msg: 'Please enter all fields'});
-  }
-  
-  console.log(req.body);  
-});
+router.post('/:id',user.getUserByID);
+router.post('/update/email/:id', user.updateEmail);
+router.post('/update/username/:id', user.updateUsername);
+router.post('/update/first_name/:id', user.updateFirstName);
+router.post('/update/last_name/:id', user.updateLastName);
+router.post('/update/telephone/:id', user.updateTelephone);
+router.post('/update/password/:id', user.updatePassword);
 
 
-// @route   POST api/users/login
-// @desc    Login user and return JWT token
-// @access  Public
-router.post('/signin', (req, res) => {
-  console.log(req.body);
-  
-});
-
-
-// export router
 module.exports = router;
