2c18ce86bac71d4ec2d82887c9479b7ea91cfe2d
adeel838 <24100258@lums.edu.pk>
2023-04-02 17:28:32 +0500

    working shopping cart

---
+++ shoppingCart/frontend/src/products.js
@@ -0,0 +1,83 @@
+import product1 from "./assets/1.jpg";
+import product2 from "./assets/2.jpg";
+import product3 from "./assets/3.jpg";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.jpg";
+import product7 from "./assets/7.jpg";
+import product8 from "./assets/8.jpg";
+import product9 from "./assets/9.jpg";
+import product10 from "./assets/10.jpg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        name: "AmazonCommercial Premium 100% Cotton Bath Mat Set",
+        price: 1.99,
+        image: product1,
+        //description: "A red apple",
+    },
+    {
+        id: 2,
+        name: "Aonee Dish Drying Rack",
+        price: 1.99,
+        image: product2,
+        //description: "A red apple",
+    },
+    {
+        id: 3,
+        name: "Rustic Soap Dish Saver Bath Draining Holder Bathroom Tray",
+        price: 1.99,
+        image: product3,
+        //description: "A red apple",
+    },
+    {
+        id: 4,
+        name: "Christopher Knight Home Luna Acacia Wood Night Stand, Brown",
+        price: 1.99,
+        image: product4,
+        //description: "A red apple",
+    },
+    {
+        id: 5,
+        name: "Deco 79 Contemporary Ceramic Cylinder Vase, White",
+        price: 1.99,
+        image: product5,
+        //description: "A red apple",
+    },
+    {
+        id: 6,
+        name: "Deco 79 Bamboo Tall Woven Floor Vase, White",
+        price: 1.99,
+        image: product6,
+        //description: "A red apple",
+    },
+    {
+        id: 7,
+        name: "Elly Décor 12-inch Square Plastic Planter with Drainage Hole, Plant Pot",
+        price: 1.99,
+        image: product7,
+        //description: "A red apple",
+    },
+    {
+        id: 8,
+        name: "HENCKELS Statement Razor-Sharp",
+        price: 1.99,
+        image: product8,
+        //description: "A red apple",
+    },
+    {
+        id: 9,
+        name: "Mr. Stacky 5-Tier Strawberry Planter Pot, 5 Pots",
+        price: 1.99,
+        image: product9,
+        //description: "A red apple",
+    },
+    {
+        id: 10,
+        name: "VECELO Modern Versatile Nightstands X-Design Storage Shelf",
+        price: 1.99,
+        image: product10,
+        //description: "A red apple",
+    },
+]
