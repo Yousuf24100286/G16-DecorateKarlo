d800c67958b23d0817bf2b92a51a12d0fd378362
Yousuf24100286 <24100286@lums.edu.pk>
2023-04-15 12:20:30 +0500

    Deployment Configuration II

--- backend/database/config.js
+++ backend/database/config.js
@@ -3,21 +3,27 @@ require('dotenv').config();
 module.exports = 
 {
   "development": {
-    url: process.env.DEV_PGDATABASE_URL,
-    "dialect": "postgres"
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
   },
   "test": {
-    "username": "root",
-    "password": "olraketaroced",
-    "database": "decoratekarlo",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
   },
   "production": {
-    "username": "root",
-    "password": "olraketaroced",
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
   }
 }
